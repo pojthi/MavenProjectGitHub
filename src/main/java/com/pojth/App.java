42a56019ecdd2824d9bbb8e50cd6dc25d08e906f
Poj,Thiraphapwiwat <Pojthi@gmail.com>
2020-08-26 09:31:18 +0700

    First Commit

---
+++ src/main/java/com/pojth/App.java
@@ -0,0 +1,13 @@
+package com.pojth;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
